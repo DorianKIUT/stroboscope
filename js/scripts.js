e8da1c2bd4c65c80b80ec545500f445a8df50b21
DorianKIUT <dorian.kuszli@etu.univ-nantes.fr>
2017-12-12 19:44:54 +0100

    commentaire de script.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,28 +4,34 @@ var index = 0;
 
 function startOrStop()
 {
+	//à l'initialisation, la valeur est à false, ce qui fait que quand on appuie sur le bouton Start/stop
+	// la valeur devient true ( l'inverse ). Et au contraire quand le bouton est à "true", il passe à "false" quand on réapuie dessus.
 	isStarted = !isStarted;
-
+	//si le bouton est à true
 	if (isStarted)
-	{
-		interval = setInterval(color, 100);
+	{	
+		//execution de la fonction color pendant 100ms
+		interval = setInterval(color, 10;
 	}
-
+	//si le bouton est à false:
 	else
 	{
+		//on supprime l'intervalle (réinitialise) et on remet blanche la couleur du background.
 		clearInterval(interval);
 		document.body.style.backgroundColor = 'white';
 	}
 
 }	
-
+//fonction qui va définir la couleur de fond du body de la page html
 function color()
 {
+	//la couleur de background est égale au résultat de l'execution de la fonction randomColor()
 	document.body.style.backgroundColor = randomColor();
 }
 
 function randomColor()
 {
+	//on initialise un tableau contenant des couleurs
 	var arrayColor = [
 		'blue',
 		'red',
@@ -36,7 +42,10 @@ function randomColor()
 		'purple'
 
 	];
+	//On parcourt le tableau à l'aide d'un index et au modulo de sa taille ( on crée une boucle infinie )
 	var colorBody = arrayColor[index  % arrayColor.length];
+	//on incrémente l'index
 	index++;
+	//on retourne la couleur correspondante
 	return colorBody;
 }
